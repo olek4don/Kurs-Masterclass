156c7ca0b230a336f955118cd811bbd94198da82
Axel Ef <olek4don@gmail.com>
2022-09-20 20:37:25 +0200

    composition and aggregation

--- ducks.py
+++ ducks.py
@@ -1,4 +1,21 @@
+class Wing(object):
+    
+    def __init__(self, ratio):
+        self.ratio = ratio
+
+    def fly(self):
+        if self.ratio > 1:
+            print("Weee, this is fun")
+        elif self.ratio == 1:
+            print("This is hard work, but I'm flying")
+        else:
+            print("I think I'll just walk")
+        
+
 class Duck(object):
+
+    def __init__(self):
+        self._wing = Wing(1.8)
     
     def walk(self):
         print("Waddle, waddle, waddle")
@@ -9,6 +26,8 @@ class Duck(object):
     def quack(self):
         print("Quack, quack")
 
+    def fly(self):
+        self._wing.fly()
 
 class Penguin(object):
     
@@ -22,15 +41,15 @@ class Penguin(object):
         print("Are you 'avin' a larf? I'm a penguin!")
 
 
-def test_duck(duck):
-    duck.walk()
-    duck.swim()
-    duck.quack()
+# def test_duck(duck):
+#     duck.walk()
+#     duck.swim()
+#     duck.quack()
     
 if __name__ == '__main__':
     donald = Duck()
-    test_duck(donald)
+    donald.fly()
 
-    percy = Penguin()
-    test_duck(percy)
-    
+    # percy = Penguin()
+    # test_duck(percy)
+  
